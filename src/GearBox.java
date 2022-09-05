51655fce5a537c247f539df10d256cc22a335f38
User <dg21stew@gmail.com>
2022-09-05 16:36:09 -0700

    added methods: operateClutch addGear changeGear wheelSpeed

--- src/GearBox.java
+++ src/GearBox.java
@@ -6,15 +6,47 @@ public class GearBox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public GearBox(int maxGears) {
         this.maxGears = maxGears;
         this.gears = new ArrayList<Gear>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
 
     }
 
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if ((number > 0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if (newGear >= 0 && newGear < this.gears.size() && this.clutchIsIn){
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Grrrrind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs){
+        if (clutchIsIn) {
+            System.out.println("Vrrrooom Vrrroom!!");
+            return 0.0;
+        }
+        return revs*gears.get(currentGear).getRatio();
+    }
+
+
     //Inner class
-    public class Gear {
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
@@ -22,10 +54,14 @@ public class GearBox {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
         }
+        public double getRatio(){
+            return ratio;
+        }
 
         public double driveSpeed(int revs) {
             return revs * (this.ratio);
         }
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,19 @@ public class Main {
         GearBox ae86 = new GearBox(5);
 
         //Reference outer class first, then inner class, object name, inner class name and parameters
-        GearBox.Gear first = ae86.new Gear(1, 3.587);
-        GearBox.Gear second = ae86.new Gear(2, 2.022);
-        GearBox.Gear third = ae86.new Gear(3, 1.384);
-        GearBox.Gear fourth = ae86.new Gear(4, 1.00);
-        GearBox.Gear fifth = ae86.new Gear(5, 0.861);
-
-        System.out.println(first.driveSpeed(1000));
+        ae86.addGear(1, 3.587);
+        ae86.addGear(2, 2.022);
+        ae86.addGear(3, 1.384);
+        ae86.addGear(4, 1.00);
+        ae86.addGear(5, 0.861);
+        ae86.operateClutch(true);
+        ae86.changeGear(1);
+        ae86.operateClutch(false);
+        System.out.println(ae86.wheelSpeed(1000));
+        ae86.operateClutch(true);
+        ae86.changeGear(2);
+        ae86.operateClutch(false);
+        System.out.println(ae86.wheelSpeed(1000));
 
 
     }
